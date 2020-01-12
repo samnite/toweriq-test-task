89e95bbc2769014c5aee42d5368e058d209ee36d
Alexander Gribenchenko <samnite87@gmail.com>
2020-01-12 19:45:18 +0300

    remove unnecessary comments

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line
 export const createReducer = (initialState, handlers) => {
   return function reducer(state = initialState, action) {
-    // eslint-disable-next-line no-prototype-builtins
     if (handlers.hasOwnProperty(action.type)) {
       return handlers[action.type](state, action);
     }
